4c4c6bbed250fbcad2afa403626aab499a05f76e
Nelia11 <levit.nelia@icloud.com>
2024-11-27 19:28:22 +0100

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Flex, Image, useAuthenticator } from '@aws-amplify/ui-react'
+import logo from '../src/assets/logo.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { signOut } = useAuthenticator();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Flex
+      direction='column'
+      alignItems="space-between"
+      minHeight="100vh"
+    >
+      <Flex
+        as="header"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="1rem"
+        backgroundColor="darkgreen"
+      >
+        <Image src={logo} alt='logo' height={50} />
+        <Button onClick={signOut}>Sign out</Button>
+      </Flex>
+      <Flex justifyContent="center">
+        In plants we trust
+      </Flex>
+    </Flex>
   )
 }
 
